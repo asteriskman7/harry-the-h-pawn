60399946a394d20a57b04ab74186519507370ff2
Aster Isk <asterisk@mothra.localdomain>
2021-08-15 09:59:25 -0400

    allow multiple return messages, anticipate db for storage

--- Game.js
+++ Game.js
@@ -6,28 +6,29 @@
 */
 
 class Game {
-  constructor(gameOptionsString) {
+  constructor(gameid, db, gameOptionsString) {
     //init game and store options
     //return message to send with initial game state
-    return { type: 'text', content: 'Game started' };
+    this.gameid = gameid;
+    this.db = db;
+    return {responses: [{ type: 'text', content: 'Game started' }]};
   }
 
-  loadState(obj) { 
-    //convert save object into game state
+  loadState() { 
+    //get state from db or initialize
   }
 
   saveState() { 
-    //convert game state into save object and return
-    return { };
+    //convert game state into save object and save in db
   }
 
   isGameOver() { 
     //return game over state and if game is over final message
-    return { over: true, type: 'text', content: 'Game over'};
+    return { over: true, responses: [{type: 'text', content: 'Game over'}]};
   }
 
   takeTurn(playerIndex, turn) { 
     //return game state message
-    return { type: 'text', content: 'Turn taken'};
+    return {responses [{ type: 'text', content: 'Turn taken'}]};
   }
 }
