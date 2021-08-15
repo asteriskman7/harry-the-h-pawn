a3cd8e42365fbfcb25c5575a9d986f4e9fb6358e
Aster Isk <asteriskman@gmail.com>
2021-08-15 10:01:05 -0400

    get command framework started

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 "use strict";
 
 const dotenv = require('dotenv');
+const redis = require('redis');
 const {Client, Intents, MessageAttachment, ThreadManager } = require('discord.js');
 const { createCanvas } = require('canvas');
 
 dotenv.config();
+
+const db = redis.createClient({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT
+});
 
 const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
@@ -12,8 +18,67 @@ client.once('ready', () => {
   console.log('Ready!');
 });
 
+const cmdHandlers = new Map();
+
+cmdHandlers.set('new', {
+  cmd: (message, args) => {message.channel.send('create new game');},
+  help: `start a new game`
+});
+
+cmdHandlers.set('help', (() => {
+  const helpCmd = {
+    help: 'display command help'
+  };
+
+  const keyList = [];
+  cmdHandlers.forEach( (value, key) => keyList.push(key) );
+  keyList.sort();
+
+  let helpText = `\`\`\`harry-the-h-pawn manages turn based games like tic-tac-toe and chess.
+Games are spawned into new threads. Inside threads, no prefix is necessary to indicate game moves.
+Commands:
+`;
+
+  keyList.forEach( k => {
+    helpText += `\n${k}: ${cmdHandlers.get(k).help}`; 
+  });
+
+  helpText += '```';
+
+  helpCmd.cmd = (message, args) => message.channel.send(helpText);
+
+  return helpCmd;
+  })()
+);
+
 client.on('messageCreate', message => {
   //does it now include messages from itself?
+
+
+  //if author is a bot, reject
+  if (message.author.bot) {return;}
+    
+  //if in the main channel
+  if (message.channel.name === process.env.CMD_CHANNEL) {
+    console.log('process cmd');
+    const cmdList = message.content.split` `;
+    if (cmdList[0] !== 'harry') {return;}
+    
+    const cmd = cmdList[1];
+    const args = cmdList.slice(2).join` `;
+
+    if (cmdHandlers.has(cmd)) {
+      cmdHandlers.get(cmd).cmd(message, args);
+    } else {
+      message.channel.send('Unrecognized command.');
+    }
+
+  } else {
+    console.log('check channel');
+  }
+
+  /*
+  //if in a game channel dispatch msg to game
 
   console.log(message.content);
 
@@ -43,6 +108,7 @@ client.on('messageCreate', message => {
       message.channel.delete('done');
     }
   }
+  */
   /*
   if (message.content === 'make') {
     //message.channel.send('TMP');
@@ -54,6 +120,9 @@ client.on('messageCreate', message => {
   }
   */
 });
+
+
+
 
 client.login(process.env.BOT_TOKEN);
 
